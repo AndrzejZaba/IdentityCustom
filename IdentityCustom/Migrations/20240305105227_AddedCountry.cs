729b2c4b5320c22114f181595eff49c82d296846
AndrzejZaba <andrzejzaba@onet.pl>
2024-03-05 12:44:37 +0100

    Add project files.

---
+++ IdentityCustom/Migrations/20240305105227_AddedCountry.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityCustom.Migrations
+{
+    public partial class AddedCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+        }
+    }
+}
